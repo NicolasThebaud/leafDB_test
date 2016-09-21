6266c13b2bfd5c573b3bcbf398286ed88f38e1b6
NicolasThebaud <thebaud.nicolas012@gmail.com>
2016-09-21 19:52:56 +0200

    Wrote test for querying functions

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ console.log('Server started on port', port)
 var leafdb = require('leafdb')
 leafdb.getCollection("exemple", res=>{
 	console.log(res)
+	console.log("\r\n")
+	console.log(res.find({"key1": "value1"}))
 })
